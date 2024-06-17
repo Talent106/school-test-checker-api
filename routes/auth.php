cc9b8023ae4566e074b0fd5be401a015e37955ea
Talent106 <kimurayu106@outlook.com>
2024-06-17 00:24:26 -0300

    feat: remove unused middleware and set login route

--- routes/auth.php
+++ routes/auth.php
@@ -6,8 +6,7 @@ use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [LoginController::class, 'authenticate'])->middleware('guest')->name('login');
-
+Route::post('/login', [AuthController::class, 'login'])->name('login');
