03efa710fab22aa4f504f2e64aea41950eb86206
Talent106 <kimurayu106@outlook.com>
2024-06-24 16:04:42 -0300

    feat: create tables

---
+++ database/migrations/2024_06_24_113539_create_attachments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attachments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('original_file_name');
+            $table->string('original_file_type');
+            $table->string('original_file_size');
+            $table->string('file_path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attachments');
+    }
+};
---
+++ database/migrations/2024_06_24_114027_create_attachables_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attachables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attachment_id')->constrained();
+            $table->foreignId('attachable_id');
+            $table->string('attachable_type');
+            $table->index(['attachable_id', 'attachable_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attachables');
+    }
+};
